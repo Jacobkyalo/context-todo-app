941587657f6070e9f3b9b7554d2bc51b9cc88ebd
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-03-09 21:26:15 +0300

    rendered components and context

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { Header } from "./components/Header";
+import { Form } from "./components/Form";
+import { Todos } from "./components/Todos";
+import { TodoContextProvider } from "./contexts/TodoContext";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-6xl text-blue-700">hello vite</h1>
-    </div>
+    <TodoContextProvider>
+      <div className="w-full pt-8 h-[100vh] bg-slate-800 overflow-hidden flex justify-start items-center flex-col">
+        <Header title="Context Todo Application" />
+        <Form />
+        <Todos />
+      </div>
+    </TodoContextProvider>
   );
 }
 
