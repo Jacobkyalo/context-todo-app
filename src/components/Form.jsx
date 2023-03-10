08785289269689a6a40f0362130abf076eff3e5c
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-03-10 09:14:05 +0300

    implemented react-hook-form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,35 +1,42 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
+import { useForm } from "react-hook-form";
 import { v4 as uuidv4 } from "uuid";
 import { IoMdAdd } from "react-icons/io";
 import { TodoContext } from "../contexts/TodoContext";
 
-export const Form = ({ addTodo }) => {
-  const [text, setText] = useState("");
+export const Form = () => {
   const { dispatch } = useContext(TodoContext);
 
-  const handleAddTodo = (e) => {
-    e.preventDefault();
-    if (!text || text.length < 4) return;
-    dispatch({
-      type: "addTodo",
-      id: uuidv4(),
-      text: text,
-    });
-    setText("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      text: "",
+    },
+  });
+  const onSubmit = (data) => {
+    dispatch({ type: "addTodo", id: uuidv4(), text: data.text });
+    data.text = "";
   };
+
   return (
     <>
       <form
-        onSubmit={handleAddTodo}
+        onSubmit={handleSubmit(onSubmit)}
         className="w-[300px] border-2 border-green-400 rounded-lg p-6"
       >
         <div className="flex justify-center items-center">
           <input
             type="text"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
+            {...register("text", {
+              required: true,
+              minLength: 4,
+            })}
             className="w-[85%] p-2 rounded-sm bg-slate-600 outline-none text-white"
           />
+
           <button
             type="submit"
             className="w-[15%] p-3 ml-4 transition-all duration-500 outline-none ease-in bg-green-400 hover:bg-green-300 flex justify-center items-center rounded-sm text-base"
@@ -37,6 +44,9 @@ export const Form = ({ addTodo }) => {
             <IoMdAdd color="#000" />
           </button>
         </div>
+        {errors.text && (
+          <p className="text-red-600">Enter a valid text or todo</p>
+        )}
       </form>
     </>
   );
